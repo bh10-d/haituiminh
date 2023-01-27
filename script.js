0677fd2d17c29bd42d31db858830e20327696722
bh10-d <duchieubui25012002@gmail.com>
2023-01-27 15:38:24 +0700

    build themes

--- script.js
+++ script.js
@@ -13,14 +13,31 @@
     let moutains_behind = document.getElementById('moutains_behind');
     let mountains_front = document.getElementById('mountains_front');
     let play = document.getElementById('play');
+    let section = document.getElementById('body_sec');
+    // let section_block = document.getElementsByClassName('sub_main');
     // let text = document.getElementById('text');
-    
-    console.log(play.videoHeight)
-    
+
+
+    let screenHeight = window.screen.height; //chia cho 100 la ra dc 1vh
+    let screenWidth = window.screen.width;
+
+    play.addEventListener('loadedmetadata', ()=>{
+        let videoHeight = play.videoHeight;
+        // let videoWidth = play.videoWidth;
+        
+        if(screenHeight >= 1080){
+            section.style.height = 110+'vh';
+        }else{
+            section.style.height = 45 + videoHeight/(screenHeight/100) + 'vh';
+        }
+        // console.log(videoHeight/(screenHeight/100))
+        // console.log(section)
+    })
+
     window.addEventListener('scroll', ()=>{
         let value = window.scrollY
-        stars.style.left = value * 0.25 + 'px';
-        moon.style.top = value * 1.05 + 'px';
+        stars.style.left = value * 0.25555555555555 + 'px';
+        moon.style.top = value * 1.0555555555555555 + 'px';
         moutains_behind.style.top = value * 0.5 + 'px';
         mountains_front.style.top = value * 0 + 'px';
         // text.style.marginRight = value * 2.7 + 'px';
