1d01b692ba825a9659026209ad0cf9f7f1ea35c1
buiduchieu <duchieubui25012002@gmail.com>
2023-08-04 14:39:18 +0700

    fix upload content no media and reset data test and push imagegrid to comming soon

--- js/upload.js
+++ js/upload.js
@@ -53,76 +53,90 @@ file.addEventListener("change", (event) => {
 
 function uploadFile(file) {
 const storageRef = ref(storage, `hieuthao/${file.name}`);
-
 const uploadTask = uploadBytesResumable(storageRef, file);
-uploadTask.on('state_changed', 
-    (snapshot) => {
-    // Observe state change events such as progress, pause, and resume
-    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log('Upload is ' + progress + '% done');
-    percents.innerHTML = parseInt(progress)+"%";
-    switch (snapshot.state) {
-        case 'paused':
-        console.log('Upload is paused');
-        break;
-        case 'running':
-        console.log('Upload is running');
-        break;
-    }
-    }, 
-    (error) => {
-    // Handle unsuccessful uploads
-    }, 
-    () => {
-    // Handle successful uploads on complete
-    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            dataImage.push(downloadURL);
-            // console.log('File available at', downloadURL);
-            // console.log(dataImage);
-            if(dataImage.length == dataPreviousImage.length){
-                let convertImage = '';
-                let convertVideo = '';
-                // console.log(typeof file.type)
-                if(file.type.includes("image")){
-                    for(let i = 0; i < dataImage.length; i++){
-                        convertImage += `<img width="95%" height="100%" src="${dataImage[i]}" alt="">`
+console.log(file)
+if(file != []){
+    uploadTask.on('state_changed', 
+        (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        percents.innerHTML = parseInt(progress)+"%";
+        switch (snapshot.state) {
+            case 'paused':
+            console.log('Upload is paused');
+            break;
+            case 'running':
+            console.log('Upload is running');
+            break;
+        }
+        }, 
+        (error) => {
+        // Handle unsuccessful uploads
+        }, 
+        () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                dataImage.push(downloadURL);
+                // console.log('File available at', downloadURL);
+                // console.log(dataImage);
+                if(dataImage.length == dataPreviousImage.length){
+                    let convertImage = '';
+                    let convertVideo = '';
+                    // console.log(typeof file.type)
+                    if(file.type.includes("image")){
+                        for(let i = 0; i < dataImage.length; i++){
+                            convertImage += `<img width="95%" height="100%" src="${dataImage[i]}" alt="">`
+                        }
                     }
-                }
-                if(file.type.includes("video")){
-                    for(let i = 0; i < dataImage.length; i++){
-                        convertVideo += `
-                            <video width="100%" controls id="play">
-                                <source src="${dataImage[i]}" type="video/mp4">
-                            </video>`
+                    if(file.type.includes("video")){
+                        for(let i = 0; i < dataImage.length; i++){
+                            convertVideo += `
+                                <video width="100%" controls id="play">
+                                    <source src="${dataImage[i]}" type="video/mp4">
+                                </video>`
+                        }
                     }
-                }
-                // console.log(convertImage);
-                // if(date.value != ""){
-                //     console.log(date.valueAsDate.getDate()+'/'+month_data[date.valueAsDate.getMonth()]+'/'+date.valueAsDate.getFullYear());
-                // }
-                // console.log(g_date.getDate()+'/'+month_data[g_date.getMonth()]+'/'+g_date.getFullYear());
-                if(myEditor.getData() != "" ){
-                    let data = {
-                        date: date,
-                        content: myEditor.getData(),
-                        image: convertImage,
-                        video: convertVideo
+                    // console.log(convertImage);
+                    // if(date.value != ""){
+                    //     console.log(date.valueAsDate.getDate()+'/'+month_data[date.valueAsDate.getMonth()]+'/'+date.valueAsDate.getFullYear());
+                    // }
+                    // console.log(g_date.getDate()+'/'+month_data[g_date.getMonth()]+'/'+g_date.getFullYear());
+                    if(myEditor.getData() != "" ){
+                        let data = {
+                            date: date,
+                            content: myEditor.getData(),
+                            image: convertImage,
+                            video: convertVideo
+                        }
+                        uploadData({...data})
                     }
-                    uploadData({...data})
+                    console.log('successful')
                 }
-                console.log('successful')
-            }
-        });
-    }
-);
+            });
+        }
+    );
+}
 }
 //end upload image
 
 
 submit.addEventListener('click', () => {
-    dataPreviousImage.map(file=>uploadFile(file)); 
+    if(dataPreviousImage.length != 0){
+        dataPreviousImage.map(file=>uploadFile(file)); 
+    }else{
+        if(myEditor.getData() != "" ){
+            let data = {
+                date: date,
+                content: myEditor.getData(),
+                image: "",
+                video: ""
+            }
+            uploadData({...data})
+        }
+    }
     isLoading.classList.remove('hidden'); //isLoading
 });
 
@@ -137,7 +151,11 @@ async function uploadData({date, content, image, video}){
             video: video
         });
         console.log("Document written with ID: ", docRef.id);
-        isLoading.classList.add("hidden");
+        setTimeout(() => {
+            isLoading.classList.add("hidden");
+            window.location.replace("/")
+        }, 1500);
+        
       } catch (e) {
         console.error("Error adding document: ", e);
       }
